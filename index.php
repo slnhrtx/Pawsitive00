f0894a4c048df2f38930c875a4ad8bdfcfec6cfc
Dan Vincent Teodoro <131141374+MagicTurtle002@users.noreply.github.com>
2025-02-05 23:05:20 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pawsitive</title>
+  <link rel="icon" type="image/x-icon" href="assets/images/logo/LOGO.png">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
+
+  <link rel="stylesheet" href="assets/css/index.css">
+</head>
+
+<body>
+  <header>
+    <nav>
+      <div class="logo">
+        <img src="assets/images/logo/LOGO 2 WHITE.png" alt="Pawsitive Logo">
+      </div>
+      <ul class="nav-links">
+        <li><a href="public/staff_login.php">Log in</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <br>
+    <br>
+    <section class="hero">
+      <div class="hero-text">
+        <h1>Your Pet's Care</h1>
+        <h2>Our Priority</h2>
+        <p>We're dedicated to enhancing the lives of pets<br>through expert care and heartfelt commitment.</p>
+        <br>
+        <br>
+        <br>
+      </div>
+      <img src="assets/images/Icons/For Index.png" alt="Dog and Cat">
+    </section>
+
+    <section class="about-us">
+      <section class="contact-info">
+        <div class="about-content">
+          <h2>About Us</h2>
+          <p>
+            At PAWSITIVE, we believe that every pet deserves the best care and attention. Established with a passion for
+            animal
+            welfare, we aim to provide top-notch tech service to ensure your furry friends lead healthy and happy lives.
+          </p>
+          <br>
+          <p>
+            Our team of aspiring developers with the help of Pet Adventure staff are committed to offering compassionate
+            care
+            tailored to the unique needs of every pet. Whether it's a routine check-up, specialized treatment,
+            or quality pet supplies, we're here to support you every step of the way.
+          </p>
+        </div>
+        <br>
+        <div class="mission-vision">
+          <div class="card mission">
+            <h2>Mission</h2>
+            <p>
+              To empower pet owners and veterinary professionals through innovative technology,
+              fostering healthier and happier lives for pets while strengthening the bond between humans and their
+              animal companions.
+            </p>
+          </div>
+          <div class="card vision">
+            <h2>Vision</h2>
+            <p>
+              Our vision is to be the leading platform for seamless pet care management, and setting the standard for
+              compassionate wellness solutions.
+              We aim to empower pet owners and veterinary professionals to ensure every pet lives a healthy life.
+            </p>
+          </div>
+        </div>
+      </section>
+    </section>
+    <br>
+    <div class="dogcat-pic">
+      <img src="assets/images/Icons/Pet pic 4.png" alt="Dog and Cat">
+    </div>
+  </main>
+
+  <footer>
+    <div class="footer-content">
+      <div class="footer-logo">
+        <img src="assets/images/logo/LOGO 2 WHITE.png" alt="Pawsitive Logo">
+        <div class="logo-spacer"></div>
+        <ul class="social-media">
+          <br>
+          <li><a href="#"><img src="assets/images/Icons/Facebook.png" alt="Facebook Icon">petadventure14</a></li>
+          <li><a href="#"><img src="assets/images/Icons/Email.png" alt="Email Icon">petadventure20@gmail.com</a></li>
+          <li><a href="#"><img src="assets/images/Icons/Contact Number.png" alt="Phone Icon">0923 528 3253</a></li>
+        </ul>
+      </div>
+      <div class="footer-section">
+        <h4>Company</h4>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Appointment</a></li>
+          <li><a href="#">Contacts</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </div>
+      <div class="footer-section">
+        <h4>Help Center</h4>
+        <ul>
+          <li><a href="#">Call Center</a></li>
+          <li><a href="#">FAQs</a></li>
+          <li><a href="#">Support Docs</a></li>
+          <li><a href="#">Careers</a></li>
+        </ul>
+      </div>
+      <div class="footer-section address">
+        <h4>Address</h4>
+        <p>Stall 1, Atilano Bldg, National Road, Cay Pomba, Sta. Maria, Bulacan, 3022</p>
+      </div>
+    </div>
+    <p class="footer-bottom">&copy; 2025 Pet Adventure Veterinary Services and Supplies, All Rights Reserved.</p>
+  </footer>
+  <script src="assets/js/index.js"></script>
+</body>ß
+</html>
